651a06bbc17b6c263430d5fd7c2741a18028a20e
zhengshuaijie1 <zhengshuaijie1@gmail.com>
2025-01-26 15:13:14 +0800

    初始化项目1

--- app/src/main/java/com/example/myfirstdemo/FragmentActivity.kt
+++ app/src/main/java/com/example/myfirstdemo/FragmentActivity.kt
@@ -1,15 +1,18 @@
 package com.example.myfirstdemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import androidx.activity.ComponentActivity
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.example.myfirstdemo.databinding.FragmentActivityBinding
 
 class FragmentActivity : FragmentActivity() {
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+    private lateinit var mCities: Array<String>
     private lateinit var mbinding: FragmentActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +23,22 @@ class FragmentActivity : FragmentActivity() {
 
     private fun initView() {
 
-        val mCities = resources.getStringArray(R.array.cities)
+        mCities = resources.getStringArray(R.array.cities)
+//        val mCities = listOf("Beijing", "Shanghai", "Guangzhou")
 
-//        val arrayAdapter = ArrayAdapter(this, R.id.sp_city, mCities)
+        arrayAdapter = ArrayAdapter<String>(this, R.layout.fragment_activity, mCities)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
 //        mbinding.spCity.adapter = arrayAdapter
 
-//        mbinding.spCity.onItemSelectedListener = OnItemSelectedListener()
+        mbinding.spCity.onItemSelectedListener = object :OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Toast.makeText(this@FragmentActivity,"你好",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
